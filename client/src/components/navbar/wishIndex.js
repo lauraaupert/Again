b7fed55e4326469c8dbf8be020bff983450941ba
Matthew Vanni <mvanni4994@gmail.com>
2021-04-25 16:25:06 -0400

    fixed console errors

--- client/src/components/navbar/wishIndex.js
+++ client/src/components/navbar/wishIndex.js
@@ -4,7 +4,6 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -29,10 +28,8 @@ const useStyles = makeStyles((theme) => ({
     return (
       <div className={classes.root}>
         <AppBar position="static">
-          <Toolbar style={NavStyle}>
-            <Typography variant="h6" className={classes.title}>
+          <Toolbar style={NavStyle}> 
             <h1 style={{textAlign: 'center', fontFamily: "Kaushan Script", margin:"auto"}}> GameDex </h1>
-            </Typography>
           </Toolbar>
         </AppBar>
       </div>
--- client/src/components/searchbar/index.js
+++ client/src/components/searchbar/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Box from "../Box"
 
 export default function SearchBar(props) {
