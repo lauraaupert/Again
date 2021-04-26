99161aebe287494b329f46b915d95448b54b48f5
Laura Aupert <aupert.laura@gmail.com>
2021-04-26 19:54:47 -0400

    no more build

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -22,8 +22,8 @@ export default {
     return axios.delete("/api/users/" + id);
   },
   // Saves a game to the database
-  saveGame: function (gameData) {
-    return axios.post("/api/users", gameData);
+  saveGame: function (id, gameData) {
+    return axios.post("/api/users" + id, gameData);
   },
 
   // Saves a user to the database
