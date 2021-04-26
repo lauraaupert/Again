479f621002063988901d1ef3b5bf51a01a99fefe
Rajahni fields <rajahnifields@gmail.com>
2021-04-26 16:53:04 -0400

    merging

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -35,23 +35,9 @@ export default function GameCard() {
           color="textSecondary"
           gutterBottom
         >
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+          Game List
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
