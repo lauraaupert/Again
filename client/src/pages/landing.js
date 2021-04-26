70a17af569bbd2e65c212d50c65c3b237eaaca8a
Rajahni fields <rajahnifields@gmail.com>
2021-04-26 16:40:01 -0400

    merging problems

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -2,70 +2,69 @@ import React, { useEffect, useState } from "react";
 import SearchBar from "../components/searchbar";
 import API from "../utils/API";
 import Results from "../components/Results";
-import Wrapper from "../components/Wrapper"
-import VideoBg from "../components/VideoBg/index"
+import Wrapper from "../components/Wrapper";
+import VideoBg from "../components/VideoBg/index";
 
-import { List } from "../components/List"
+import { List } from "../components/List";
 import Navbar from "../components/Navbar/index.js";
-
-
+import GameCard from "../components/Card";
 
 function Landing() {
-
-
   const [games, setGames] = useState({ results: [] });
   const [userSearch, setUserSearch] = useState("");
 
-    // useEffect(() => searchGames(), []);
+  // useEffect(() => searchGames(), []);
 
-    function handleInputChange(event) {
-        const { value } = event.target;
-        console.log(value);
-        setUserSearch(value);
-    };
+  function handleInputChange(event) {
+    const { value } = event.target;
+    console.log(value);
+    setUserSearch(value);
+  }
 
-    function searchGames(query) {
-      console.log(query)
-        API.searchTerms(query)
-        
-            .then(res => {
-              console.log(res.data.results)
-                if (res.data === undefined) {
-                    setGames({ results: [] });
-                } else {
-                    setGames({ results: res.data.results })
-                }
-            })
-            .catch(err => console.log(err));
-    };
+  function searchGames(query) {
+    console.log(query);
+    API.searchTerms(query)
 
-    function handleSearchSubmit(event) {
-        event.preventDefault();
-        console.log(userSearch)
-        searchGames(userSearch);
-    };
+      .then((res) => {
+        console.log(res.data.results);
+        if (res.data === undefined) {
+          setGames({ results: [] });
+        } else {
+          setGames({ results: res.data.results });
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    console.log(userSearch);
+    searchGames(userSearch);
+  }
   return (
     <div>
       <Navbar />
       <VideoBg />
 
-    <SearchBar inputChange={handleInputChange} handler={handleSearchSubmit}/>
+      <SearchBar inputChange={handleInputChange} handler={handleSearchSubmit} />
+      <GameCard />
       <Wrapper>
-      {games.results.length ?
-        <List>
-      {games.results.map(result => (
-    <Results
-    key={result.id}
-    title={result.name}
-    platform={result.platforms[0].platform.name}
-    store={result.stores[0].store.name}
-    image={result.background_image}
-/>
-    ))}
-    </List> :
-    (<h1>Got nothing buddy</h1>)
-      }
-    </Wrapper>
+        {games.results.length ? (
+          <List>
+            {games.results.map((result) => (
+              <Results
+                key={result.id}
+                title={result.name}
+                platform={result.platforms[0].platform.name}
+                store={result.stores[0].store.name}
+                image={result.background_image}
+              />
+            ))}
+          </List>
+        ) : (
+          <h1>Got nothing buddy</h1>
+        )}
+      </Wrapper>
     </div>
   );
 }
