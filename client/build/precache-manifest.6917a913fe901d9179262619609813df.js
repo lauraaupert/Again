2c5f081f815adc85e8998c0ee4f2cc23569f2ff8
Laura Aupert <aupert.laura@gmail.com>
2021-04-27 16:56:01 -0400

    new build

---
+++ client/build/precache-manifest.6917a913fe901d9179262619609813df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae7edd69e0ad415e37310f9ccb1dc82b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770718563eece6b7fb91",
+    "url": "/static/css/main.f4fadec8.chunk.css"
+  },
+  {
+    "revision": "d5ec09245109468594ac",
+    "url": "/static/js/2.7fe8e838.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.7fe8e838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770718563eece6b7fb91",
+    "url": "/static/js/main.a8675458.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "37d48bb4162539c77f5c86c0d66a5228",
+    "url": "/static/media/gameMontage.37d48bb4.mp4"
+  }
+]);
