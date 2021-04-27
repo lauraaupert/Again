c7c3bcb9da1d042660bd35d4eadc73048d8a17bf
Laura Aupert <aupert.laura@gmail.com>
2021-04-27 13:20:18 -0400

    Container in list component

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,10 +1,24 @@
 import React from "react";
-// This file exports both the List and ListItem components
+import CssBaseLine from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
 
 export function List({ children }) {
   return (
     <div className="list-overflow-container">
+          <React.Fragment>
+        <CssBaseLine>
+        <Container fixed>
+        <Typography component="div" style={{ backgroundColor: 'rgb(245 245 245 / 44%)', height: '75vh'}}>
+
       <ul className="list-group">{children}</ul>
+      </Typography>
+        
+        </Container>
+        </CssBaseLine>
+        </React.Fragment>
+
+
     </div>
   );
 }
